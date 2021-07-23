ec8d484ed8977b7f5077a8600fc8e514b7d75e9c
andreevsergey <andreev,sv89@yandex.ru>
2021-07-23 23:45:36 +0300

    remove variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   def test_level(level)
-    id = self.id
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id').where('tests.level = ?', level).where('user_tests.user_id = ?', self.id).pluck(:title)
   end
 end
