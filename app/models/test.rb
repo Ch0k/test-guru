9e802fe2c884986823e59e203cf1a456b5472afe
andreevsergey <andreev,sv89@yandex.ru>
2021-07-24 01:58:14 +0300

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+  has_many :user_tests
+  has_many :users, through: :user_tests
+  belongs_to :category
+  has_many :questions
   def self.list_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ? ', category).order(title: :desc).pluck(:title)
   end
