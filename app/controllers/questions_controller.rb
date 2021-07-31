7102e3bb2ec692c27ba97796854b662448ec7508
andreevsergey <andreev,sv89@yandex.ru>
2021-08-01 02:35:17 +0300

    add collback set_test and code refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[ show edit update destroy ]
+  before_action :set_test,  only: %i[ index create new ]
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    @questions = Question.all
-    @test = Test.find(params[:test_id])
-    render inline: '<% @questions.where(test_id: @test).each do |question| %> <br> <%= question.body %> <% end %>'
+    render inline: '<% @test.questions.each do |question| %> <br> <%= question.body %> <% end %>'
   end
 
   def show
@@ -13,24 +12,19 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @test_id = Test.find(params[:test_id])
-    @test = @test_id.id
+    @test_id = @test.id
   end
 
   def create
     @test = Test.find(params[:test_id])
-    question = @test.questions.create(question_params)
+    question = @test.questions.new(question_params)
     #question = Question.create(question_params)
-    respond_to do |format|
       if question.save
         #render plain: question.inspect
-        format.html { redirect_to test_questions_url, notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
+        redirect_to test_questions_url, notice: 'Question was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
   def destroy
@@ -44,10 +38,14 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:body,:test_id)
+    params.require(:question).permit(:body)
   end
   
   def record_not_found
     render plain: "404 Not Found", status: 404
+  end
+
+  def set_test
+    @test = Test.find(params[:test_id])
   end
 end
