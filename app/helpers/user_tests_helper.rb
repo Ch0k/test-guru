687dbc5d488daf4104726d5e24cd2c92a3d8a53a
andreevsergey <andreev,sv89@yandex.ru>
2021-08-19 23:43:11 +0300

    remove methods in UserTestHelper

--- app/helpers/user_tests_helper.rb
+++ app/helpers/user_tests_helper.rb
@@ -1,10 +1,4 @@
 module UserTestsHelper
-  def passed? 
-    if (@user_test.correct_questions_multiply_by_100)/@user_test.test_questions_count > 85
-      true
-    end
-  end
-  def success_rate
-    (@user_test.correct_questions_multiply_by_100)/@user_test.test_questions_count
-  end
+
+
 end
