1f1b4a35f82569ab50ec1ef900b3c068a0eabb22
andreevsergey <andreev,sv89@yandex.ru>
2021-08-23 14:44:46 +0300

    add email  validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :user_tests
   has_many :tests, through: :user_tests
   has_many :author_tests, class_name: "Test", foreign_key: "author_id"
+  validates :email, presence: true, uniqueness: true,  format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   has_secure_password
 
