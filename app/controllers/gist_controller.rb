4ba94c313d0c4c4b68b3a00a3b8b597071e91be2
andreevsergey <andreev,sv89@yandex.ru>
2021-09-06 18:31:13 +0300

    add gist model and controller

---
+++ app/controllers/gist_controller.rb
@@ -0,0 +1,17 @@
+class GistController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+  has_one :gist
   validates :body, presence: true
 end
